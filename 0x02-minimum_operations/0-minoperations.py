49b4e41ffd6ca1b1cdc884eaa3cb49b4f8d26e58
Caroline <carolinekimatu6@gmail.com>
2023-07-11 16:41:53 +0000

    pycodestyle adherance

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-contains function minOperations(n)
+Contains function minOperations(n)
 """
 
 
@@ -12,17 +12,17 @@ def minOperations(n: int) -> int:
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     current_chars = 1
     clipboard = 0
-    
+
     while current_chars < n:
         if n % current_chars == 0:
             clipboard = current_chars
             operations += 1
-        
-        current_chars += clipboard
+
+            current_chars += clipboard
         operations += 1
-    
-    return operations
+
+        return operations
