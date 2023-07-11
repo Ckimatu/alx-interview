0217297734cbd3c65caa610b4955cd9cc627261e
Caroline <carolinekimatu6@gmail.com>
2023-07-11 16:15:06 +0000

    corrected logic

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,23 +5,24 @@ contains function minOperations(n)
 """
 
 
-def minOperations(n):
+def minOperations(n: int) -> int:
     """
     calculates the fewest number of operations needed
     to result in exactly n H characters
     """
     if n <= 1:
         return 0
-
-    operations = 2  # Start with Copy All and Paste
-    h_count = 1  # Initial character H
-
-    while h_count < n:
-        if n % h_count == 0:
-            operations += 1  # Perform Paste operation
-            h_count *= 2
-        else:
-            operations += 2  # Perform Copy All and Paste operations
-            h_count += h_count
-
+    
+    operations = 0
+    current_chars = 1
+    clipboard = 0
+    
+    while current_chars < n:
+        if n % current_chars == 0:
+            clipboard = current_chars
+            operations += 1
+        
+        current_chars += clipboard
+        operations += 1
+    
     return operations
