ffc4527364de8d12ed5e73e447226cb291a5aa4a
Caroline <carolinekimatu6@gmail.com>
2023-07-20 08:58:23 +0000

    log parsing correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,57 +1,68 @@
 #!/usr/bin/python3
-
 """
-reads stdin line by line and computes metrics
+Solution to:
+Write a script that reads stdin line by line and computes metrics:
+
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size> 
+(if the format is not this one, the line
+must be skipped)
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+Total file size: File size: <total size>
+where <total size> is the sum of all previous <file size>
+(see input format above)
+Number of lines by status code:
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+If a status code doesn't appear or is not an integer,
+don't print anything for this status code
+format: <status code>: <number>
+status codes should be printed in ascending order
 """
 
 import sys
 
 
-def print_metrics(total_size, status_codes):
-    """
-    print the computed metrics
-    args:
-	 @total_size: total file size
-	 @status_codes: dictionary containing
-	 the counts of each status code
-    """
-    print("File size: {}".format(total_size))
-    for code, count in sorted(status_codes.items()):
-        print("{}: {}".format(code, count))
+# store the count of all status codes in a dictionary
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0}
 
-def main():
-    line_count = 0
-    total_size = 0
-    status_codes = {}
+total_size = 0
+count = 0  # keep count of the number lines counted
 
-    try:
-        for line in sys.stdin:
-            line_count += 1
-            data = line.split()
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
 
-            if len(data) >= 8:
-                """
-		check if the line has at least 8 fields. we expect:
-		<IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-		<status code> <file size>
-		"""
-                status_code = data[-2]
-                file_size = data[-1]
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
 
-                if status_code.isdigit() and file_size.isdigit():
-                    total_size += int(file_size)
+            # check if the status code received exists in the dictionary and
+            # increment its count
+            if status_code in status_codes_dict.keys():
+                status_codes_dict[status_code] += 1
 
-                    if status_code in status_codes:
-                        status_codes[status_code] += 1
-                    else:
-                        status_codes[status_code] = 1
+            # update total size
+            total_size += file_size
 
-            if line_count % 10 == 0:
-                print_metrics(total_size, status_codes)
+            # update count of lines
+            count += 1
 
-    except KeyboardInterrupt:
-        print_metrics(total_size, status_codes)
-        raise
+        if count == 10:
+            count = 0  # reset count
+            print('File size: {}'.format(total_size))
 
-if __name__ == "__main__":
-    main()
+            # print status code counts
+            for key, value in sorted(status_codes_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
