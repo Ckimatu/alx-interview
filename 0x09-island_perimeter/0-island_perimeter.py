77b516d322949bbed3af06c27a89eedfee5ad043
Caroline <carolinekimatu6@gmail.com>
2023-08-30 11:10:51 +0000

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+Contains functions that returns the perimeter of island in grid
+"""
+
+def check_val(x):
+        # check value x
+        if (x==0):
+            return 1
+        return 0
+
+def island_perimeter(grid):
+    # returns the perimeter of the island described in grid
+    
+    row = len(grid)
+    col = len(grid[0])
+    assert (1 <= row and col <= 100), "length must be between 1 and 100"
+
+    x = 0
+    for i in range(row):
+        for j in range(col):
+            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
+                                        "grid numbers must be 0 or 1"
+            if grid[i][j] == 1:
+                if i-1 < 0:
+                    x += 1
+                else:
+                    x += check_val(grid[i-1][j])
+                if j-1 < 0:
+                    x += 1
+                else:
+                    x += check_val(grid[i][j-1])
+
+                try:
+                    x += check_val(grid[i+1][j])
+                except IndexError:
+                    x += 1
+                try:
+                    x += check_val(grid[i][j+1])
+                except IndexError:
+                    x += 1
+
+    return x
