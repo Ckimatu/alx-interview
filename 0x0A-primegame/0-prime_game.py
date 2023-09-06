490eb317fc1a79516ef064205fdfeeeaaf9e0a52
Caroline <carolinekimatu6@gmail.com>
2023-09-06 14:12:02 +0000

    update solution

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,69 +1,32 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
-Prime game Module
+Module for solving prime game question 
 """
 
-
-def isprime(n: int) -> bool:
-    """Checks if 'n' is prime number"""
-    if not n:
-        return False
-    if n > 1:
-        for i in range(2, n):
-            if not n % i:
-                return False
-        return True
-
-
-def isWinner(x: int, nums: list) -> str or None:
-    """Determines the winner of a Prime Game
-    	- X: The number of rounds
-    	- Nums: The list of numbers
-    Returns: The name of the winner if possible, else None
-    """
-    maria = {"score": 0, 'name': 'Maria'}
-    ben = {"score": 0, 'name': 'Ben'}
-    active = maria
-    wait = ben
-
-    while x:
-        try:
-            take = nums[0]
-            current = [i for i in range(1, take + 1)]
-            print()
-            print('current:', current)
-
-            for j in current:
-                print(active['name'], 'playing, Score:', active['score'])
-                if isprime(j):
-                    current = [i for i in current if not i % j]
-                else:
-                    # Scoring the winner
-                    wait['score'] += 1
-                    print(wait['name'], 'Won. Score:', wait['score'])
-
-                # Swapping players
-                if active is not maria:
-                    active = maria
-                    wait = ben
-                else:
-                    active = ben
-                    wait = maria
-            # Deleting the used item
-            nums.pop(0)
-            x -= 1
-        except IndexError:
-            break
-
-    # Getting the winner
-    if maria['score'] == ben['score']:
+def isWinner(x, nums):
+    """checks for the winner"""
+    if not nums or x < 1:
         return None
-    print('Maria Final score', maria['score'])
-    print('Ben final score', ben['score'])
-    return 'Maria' if maria['score'] > ben['score'] else 'Ben'
+    max_num = max(nums)
 
-
-# Testing the function
-if __name__ == '__main__':
-    print(isWinner(3, [3, 5, 1])) # Ben
+    my_filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not my_filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            my_filter[j] = False
+    my_filter[0] = my_filter[1] = False
+    y = 0
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
